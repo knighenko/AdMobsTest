ac62f128593e047a206a3f7a428e73a2ab1beee4
User <knighenko@gmail.com>
2022-08-21 23:03:15 +0200

    Initial commit

---
+++ app/src/main/java/com/knyzhenko/admobstest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.knyzhenko.admobstest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.nativead.NativeAd;
+import com.google.android.gms.ads.nativead.NativeAdOptions;
+import com.google.android.gms.ads.nativead.NativeAdView;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    private Button button;
+    private FrameLayout frameLayout;
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdLoader adLoader = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
+                .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                    @Override
+                    public void onNativeAdLoaded(NativeAd NativeAd) {
+
+
+                        NativeAdView adView = (NativeAdView) getLayoutInflater()
+                                .inflate(R.layout.native_ad, null);
+
+                        FrameLayout frameLayout = findViewById(R.id.fl_adplaceholder);
+                        adView.setIconView(adView.findViewById(R.id.icon));
+                        ((ImageView) adView.getIconView()).setImageDrawable(
+                                NativeAd.getIcon().getDrawable());
+                        adView.getIconView().setVisibility(View.VISIBLE);
+                        frameLayout.removeAllViews();
+                        frameLayout.addView(adView);
+                    }
+                })
+                .withAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(LoadAdError adError) {
+                        // Handle the failure by logging, altering the UI, and so on.
+                    }
+                })
+                .withNativeAdOptions(new NativeAdOptions.Builder()
+                        // Methods in the NativeAdOptions.Builder class can be
+                        // used here to specify individual options settings.
+                        .build())
+                .build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+}
---
+++ app/src/main/java/com/knyzhenko/admobstest/NativeAdMy.java
@@ -0,0 +1,14 @@
+package com.knyzhenko.admobstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NativeAdMy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_native_ad);
+    }
+}
